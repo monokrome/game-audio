86e15d603bb2a0e91c1b0cd0d0167612dc446a99
Brandon R. Stoner <monokrome@monokro.me>
2011-02-21 04:23:34 -0800

    Added a file which will be used to implement an interface for audio backends

---
+++ src/audiobackend.hpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   audiobackend.hpp
+ * Author: Brandon "monokrome" Stoner
+ *
+ * Created on February 21, 2011, 4:19 AM
+ */
+
+#ifndef AUDIOBACKEND_HPP
+#define	AUDIOBACKEND_HPP
+
+class IAudioBackend
+{
+public:
+	IAudioBackend();
+	~IAudioBackend();
+};
+
+#endif	/* AUDIOBACKEND_HPP */
+
