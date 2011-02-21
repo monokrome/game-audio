4fb37aa5e177015e27ca467b628884821e0866be
Brandon R. Stoner <monokrome@monokro.me>
2011-02-21 03:57:55 -0800

    Added files that will be used to define an audio sample interface.

---
+++ src/audiosample.cxx
@@ -0,0 +1,16 @@
+/* 
+ * File:   audiosample.cxx
+ * Author: Brandon "monokrome" Stoner
+ *
+ * Created on February 21, 2011, 3:46 AM
+ */
+
+#include "audiosample.hpp"
+
+IAudioSample::IAudioSample()
+{
+}
+
+IAudioSample::~IAudioSample()
+{
+}
---
+++ src/audiosample.hpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   audiosample.hpp
+ * Author: Brandon "monokrome" Stoner <monokrome@monokro.me>
+ *
+ * Created on February 21, 2011, 3:40 AM
+ */
+
+#ifndef AUDIOSAMPLE_HPP
+#define	AUDIOSAMPLE_HPP
+
+class IAudioSample
+{
+public:
+	IAudioSample();
+	~IAudioSample();
+};
+
+#endif	/* AUDIOSAMPLE_HPP */
